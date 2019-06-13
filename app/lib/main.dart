2e9859e9fb6981d4c23879f597019d622a77b296
Jack O'Sullivan <jackos1998@gmail.com>
2019-06-13 23:59:28 +0100

    Implement state control in app

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -14,6 +14,8 @@ const SEARCH_TIME = Duration(seconds: 2);
 const EXTRA_SEARCH_TIME = Duration(milliseconds: 300);
 final DESCRIPTION_REGEX = RegExp(r'.*description=');
 
+const DEV_DURATION = Duration(milliseconds: 200);
+
 void main() => runApp(SockitApp());
 
 class SockitApp extends StatelessWidget {
@@ -97,13 +99,30 @@ class SetState extends Request<bool> {
   }
 }
 
+final _scaffoldKey = GlobalKey<ScaffoldState>();
 class Device {
-  String service, name, description;
-  int port;
-  Set<InternetAddress> addresses = LinkedHashSet();
+  static const MIN_REQ_DURATION = Duration(milliseconds: 200);
 
-  Device(this.service, this.port) {
-    this.name = service.substring(0, service.length - QUERY.length - 1);
+  final String service, name;
+  final int port;
+  final Set<InternetAddress> addresses = LinkedHashSet();
+
+  String description;
+  final _reqInProgress = ValueNotifier(false);
+  final _state = ValueNotifier(false);
+
+  Device(this.service, this.port) : name =
+    service.substring(0, service.length - QUERY.length - 1);
+  @override
+  int get hashCode => name.hashCode;
+  @override
+  bool operator ==(dynamic other) {
+    if (other is! Device) {
+      return false;
+    }
+
+    Device dev = other;
+    return dev.name == name;
   }
 
   Future<R> _makeReq<R>(Request<R> req) async {
@@ -122,8 +141,37 @@ class Device {
 
     return req.decodeResponse(res);
   }
+  Future<R> _uiReq<R>(Request<R> req, {String failure = 'Error'}) async {
+    final watch = Stopwatch()..start();
+    _reqInProgress.value = true;
+    try {
+      return await _makeReq(req);
+    } catch (err) {
+      final snackBar = SnackBar(content: Text('$failure: $err'));
+      _scaffoldKey.currentState.showSnackBar(snackBar);
+    } finally {
+      watch.stop();
+      await Future.delayed(MIN_REQ_DURATION - watch.elapsed);
+      _reqInProgress.value = false;
+    }
+  }
 
-  _extra() =>
+  Future<void> loadState() async {
+    _state.value = await _uiReq(
+      GetState(),
+      failure: 'Failed to get state of "$name"'
+    );
+  }
+  Future<void> setState(bool newState) async {
+    if (await _uiReq(
+        SetState(newState),
+        failure: 'Failed to turn "$name" ${newState ? "on" : "off"}'
+    ) != null) {
+      _state.value = newState;
+    }
+  }
+
+  _extraInfo() =>
     Wrap(
       direction: Axis.vertical,
       spacing: 2.0,
@@ -136,29 +184,40 @@ class Device {
       ],
     );
 
-  Widget build(BuildContext context) =>
-      ListTile(
-        title: Text(name),
-        subtitle: _extra(),
-        isThreeLine: true,
-        onTap: () async {
-          var message;
-          try {
-            bool newState = Random().nextBool();
-            bool oldState = await _makeReq(SetState(newState));
-            message = 'New state: $oldState, Old state: $oldState';
-          } catch (err) {
-            message = 'Failed to get state: $err';
-          }
-
-          final snackBar = SnackBar(content: Text(message));
-          Scaffold.of(context).showSnackBar(snackBar);
-        },
-      );
+  Widget build(BuildContext context, Animation<double> animation) =>
+    SlideTransition(
+      position: animation.drive(Tween<Offset>(
+        begin: Offset(1, 0),
+        end: Offset.zero,
+      )),
+      child: Card(
+        child: ListTile(
+          title: Text(name),
+          subtitle: _extraInfo(),
+          isThreeLine: description != null,
+          trailing: ValueListenableBuilder<bool>(
+            valueListenable: _reqInProgress,
+            builder: (context, rip, child) => rip ?
+              CircularProgressIndicator() :
+                ValueListenableBuilder<bool>(
+                  valueListenable: _state,
+                  builder: (context, state, child) => Switch(
+                    value: _state.value,
+                    onChanged: setState,
+                  ),
+                ),
+          ),
+          onTap: () => setState(!_state.value),
+        )
+      )
+    );
 }
+
 class _SockitHomeState extends State<SockitHome> with WidgetsBindingObserver {
+  bool _reloading = false;
   final List<Device> _devices = [];
-  final GlobalKey<RefreshIndicatorState> _refreshKey = GlobalKey<RefreshIndicatorState>();
+  final GlobalKey<RefreshIndicatorState> _refreshKey = GlobalKey();
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey();
 
   @override
   void initState() {
@@ -169,27 +228,48 @@ class _SockitHomeState extends State<SockitHome> with WidgetsBindingObserver {
     });
   }
 
-  Future<void> _reload() async {
+  Future<void> _reload(BuildContext context) async {
     setState(() {
-      _devices.clear();
+      _reloading = true;
     });
 
+    final renewed = HashSet();
     final MDnsClient mDnsClient = MDnsClient();
     await mDnsClient.start();
-    await for (PtrResourceRecord ptr in mDnsClient.lookup(ResourceRecordQuery.serverPointer(QUERY), timeout: SEARCH_TIME)) {
-      await for (SrvResourceRecord srv in mDnsClient.lookup(ResourceRecordQuery.service(ptr.domainName), timeout: EXTRA_SEARCH_TIME)) {
-        var device = _devices.firstWhere((dev) => dev.service == srv.name, orElse: () {
-          var dev = Device(srv.name, srv.port);
-          _devices.add(dev);
-          return dev;
-        });
+    await for (PtrResourceRecord ptr in mDnsClient.lookup(
+      ResourceRecordQuery.serverPointer(QUERY),
+      timeout: SEARCH_TIME)
+    ) {
+      await for (SrvResourceRecord srv in mDnsClient.lookup(
+        ResourceRecordQuery.service(ptr.domainName),
+        timeout: EXTRA_SEARCH_TIME)
+      ) {
+        var device = _devices.firstWhere(
+          (dev) => dev.service == srv.name,
+          orElse: () {
+            var dev = Device(srv.name, srv.port);
+            _devices.add(dev);
+            _listKey.currentState.insertItem(_devices.length - 1, duration: DEV_DURATION);
+            return dev;
+          },
+        );
+        final shouldLoad = renewed.add(device);
 
-        await for (IPAddressResourceRecord a in mDnsClient.lookup(ResourceRecordQuery.addressIPv4(srv.target), timeout: EXTRA_SEARCH_TIME)) {
+        await for (IPAddressResourceRecord a in mDnsClient.lookup(
+          ResourceRecordQuery.addressIPv4(srv.target),
+          timeout: EXTRA_SEARCH_TIME)
+        ) {
           setState(() {
             device.addresses.add(a.address);
+            if (shouldLoad) {
+              device.loadState();
+            }
           });
         }
-        await for (TxtResourceRecord txt in mDnsClient.lookup(ResourceRecordQuery.text(srv.target), timeout: EXTRA_SEARCH_TIME)) {
+        await for (TxtResourceRecord txt in mDnsClient.lookup(
+          ResourceRecordQuery.text(srv.target),
+          timeout: EXTRA_SEARCH_TIME)
+        ) {
           if (txt.text.trim().startsWith(DESCRIPTION_REGEX)) {
             setState(() {
               device.description = txt.text.split('=').sublist(1).join('=');
@@ -200,21 +280,46 @@ class _SockitHomeState extends State<SockitHome> with WidgetsBindingObserver {
     }
 
     mDnsClient.stop();
+    setState(() {
+      _reloading = false;
+      _devices.where((dev) => !renewed.contains(dev)).toSet().forEach((dev) {
+        int i =_devices.indexOf(dev);
+        _devices.removeAt(i);
+        _listKey.currentState.removeItem(i, dev.build, duration: DEV_DURATION);
+      });
+    });
+  }
+
+  Widget _getBody() {
+    return _devices.length != 0 || _reloading ?
+      AnimatedList(
+        key: _listKey,
+        itemBuilder: (context, position, animation) =>
+          _devices[position].build(context, animation),
+      ) : LayoutBuilder(
+        builder: (context, viewportConstraints) =>
+          SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: viewportConstraints.maxHeight,
+              ),
+              child: Center(child: const Text('No devices found'))
+            ),
+          ),
+      );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: new RefreshIndicator(
-        child: ListView.builder(
-          itemCount: _devices.length,
-          itemBuilder: (context, position) =>
-            _devices[position].build(context),
-        ),
-        onRefresh: _reload,
+        child: _getBody(),
+        onRefresh: () => _reload(context),
         key: _refreshKey,
       ),
     );
