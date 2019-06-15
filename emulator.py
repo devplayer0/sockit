e8a2158a79312a3040605de2d81cf6e488868659
Jack O'Sullivan <jackos1998@gmail.com>
2019-06-15 14:26:11 +0100

    Implement simple broadcast discovery for emulator

--- emulator.py
+++ emulator.py
@@ -4,13 +4,19 @@ import argparse
 import struct
 import socket
 import socketserver
+import threading
+import select
 
-import zeroconf
+from eventfd import EventFD
+import netifaces
 
 socketserver.TCPServer.allow_reuse_address = True
 
 MAGIC = b'SKIT'
+DISCOVERY_PORT = 40420
 
+class DiscType(Enum):
+    SEARCH = 0x00
 class ReqType(Enum):
     GET_STATE = 0x00
     SET_STATE = 0x01
@@ -22,6 +28,9 @@ class ResType(Enum):
 class ErrType(Enum):
     BAD_REQ = 0x00
     FAILED = 0x01
+
+def default_iface():
+   return netifaces.gateways()['default'][netifaces.AF_INET][1]
 
 class SockitHandler(socketserver.BaseRequestHandler):
     def _send_error(self, type_):
@@ -75,7 +84,6 @@ class SockitHandler(socketserver.BaseRequestHandler):
 
             print(f'setting name to {new_name}')
             ref.name = new_name
-            ref.reregister()
             res = struct.pack('B', ResType.OK.value)
             self.request.sendall(res)
         elif req_type == ReqType.SET_DESC:
@@ -85,34 +93,60 @@ class SockitHandler(socketserver.BaseRequestHandler):
 
             print(f'setting description to {new_desc}')
             ref.description = new_desc
-            ref.reregister()
             res = struct.pack('B', ResType.OK.value)
             self.request.sendall(res)
 
 class Emulator:
-    def __init__(self, name, description, address, port):
+    def __init__(self, name, description, iface, port):
         self.state = False
 
         self.name = name
         self.description = description
-        self.address = address
         self.port = port
 
-        self.info = None
-        self.zc = zeroconf.Zeroconf()
-        self.reregister()
+        addr_info = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]
+        self.address = addr_info['addr']
 
-    def reregister(self):
-        if self.info:
-            self.zc.unregister_service(self.info)
-        self.info = zeroconf.ServiceInfo('_sockit._tcp.local.',
-            f'{self.name}._sockit._tcp.local.', address=socket.inet_aton(self.address),
-            port=self.port, properties={'description': self.description})
-        self.zc.register_service(self.info)
+        self.disc_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.disc_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.disc_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.disc_socket.bind((addr_info['broadcast'], DISCOVERY_PORT))
 
+        self.disc_thread = threading.Thread(target=self._run_discovery)
+        self.disc_thread.start()
+
+    def _run_discovery(self):
+        self.should_stop = EventFD()
+        while True:
+            r, _, _ = select.select([self.should_stop, self.disc_socket], [], [])
+            if self.should_stop in r:
+                break
+
+            msg, src = self.disc_socket.recvfrom(4096)
+            if not msg.startswith(MAGIC):
+                print(f'ignoring packet from {src} with invalid magic')
+                continue
+
+            try:
+                disc_type, = struct.unpack('B', msg[len(MAGIC):len(MAGIC)+1])
+                disc_type = DiscType(disc_type)
+            except Exception as ex:
+                print(f'ignoring invalid discovery request from {src}: {ex}')
+                continue
+
+            if disc_type == DiscType.SEARCH:
+                print(f'sending discovery info to {src}')
+                name_enc = self.name.encode('utf-8')
+                res = socket.inet_aton(self.address) + \
+                    struct.pack('!HB', self.port, len(name_enc)) + \
+                    name_enc + \
+                    self.description.encode('utf-8')
+                self.disc_socket.sendto(res, src)
+
+        self.disc_socket.close()
     def run(self):
-        print(f'binding on {self.address}:{self.info.port}')
-        with socketserver.TCPServer((self.address, self.info.port), SockitHandler) as server:
+        print(f'binding on {self.address}:{self.port}')
+        with socketserver.TCPServer((self.address, self.port), SockitHandler) as server:
             server.ref = self
             try:
                 server.serve_forever()
@@ -123,19 +157,19 @@ class Emulator:
     def __enter__(self):
         return self
     def __exit__(self, ex_type, ex_value, ex_traceback):
-        print('unregistering...')
-        self.zc.close()
+        print('shutting down...')
+        self.should_stop.set()
 
 def main():
     parser = argparse.ArgumentParser(description='Sockit device emulator',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--interface', default=default_iface(), help='Bind interface')
     parser.add_argument('-p', '--port', type=int, default=40420, help='Bind port')
     parser.add_argument('-n', '--name', default='My Sockit', help='Device name')
     parser.add_argument('-d', '--description', default='My first Sockit', help='Device description')
     args = parser.parse_args()
 
-    address = socket.gethostbyname(socket.getfqdn())
-    with Emulator(args.name, args.description, address, args.port) as emulator:
+    with Emulator(args.name, args.description, args.interface, args.port) as emulator:
         emulator.run()
 
 if __name__ == '__main__':
