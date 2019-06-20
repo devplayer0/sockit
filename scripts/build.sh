adf90eccd1b969b829595ce3464b31ab6674f975
Jack O'Sullivan <jackos1998@gmail.com>
2019-06-20 22:52:04 +0100

    Firmware: Rename built image to `sockit-esp8266.bin`

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,4 @@ source "$DIR/lfs_info.sh"
 cd "$DIR/../firmware"
 make
 ./luac.cross.int -a $LFS_MAPPED -o bin/lfs.img local/lua/*.lua
-srec_cat -output bin/nodemcu.bin -binary bin/0x00000.bin -binary -fill 0xff 0x00000 0x10000 bin/0x10000.bin -binary -offset 0x10000 -exclude -within bin/lfs.img -binary -offset $LFS_BASE bin/lfs.img -binary -offset $LFS_BASE
+srec_cat -output bin/sockit-esp8266.bin -binary bin/0x00000.bin -binary -fill 0xff 0x00000 0x10000 bin/0x10000.bin -binary -offset 0x10000 -exclude -within bin/lfs.img -binary -offset $LFS_BASE bin/lfs.img -binary -offset $LFS_BASE
