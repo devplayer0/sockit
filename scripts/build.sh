128f439c03affb3785131b104ce3d9fb415e6703
Jack O'Sullivan <jackos1998@gmail.com>
2019-06-20 23:12:28 +0100

    Firmware: Add option to build script to disable embedding LFS

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$DIR/lfs_info.sh"
 cd "$DIR/../firmware"
 make
-./luac.cross.int -a $LFS_MAPPED -o bin/lfs.img local/lua/*.lua
-srec_cat -output bin/sockit-esp8266.bin -binary bin/0x00000.bin -binary -fill 0xff 0x00000 0x10000 bin/0x10000.bin -binary -offset 0x10000 -exclude -within bin/lfs.img -binary -offset $LFS_BASE bin/lfs.img -binary -offset $LFS_BASE
+
+CAT_COMMAND="srec_cat -output bin/sockit-esp8266.bin -binary bin/0x00000.bin -binary -fill 0xff 0x00000 0x10000 bin/0x10000.bin -binary -offset 0x10000"
+if [ "$1" != "no_lfs" ]; then
+	./luac.cross.int -a $LFS_MAPPED -o bin/lfs.img local/lua/*.lua
+	CAT_COMMAND="$CAT_COMMAND -exclude -within bin/lfs.img -binary -offset $LFS_BASE bin/lfs.img -binary -offset $LFS_BASE"
+fi
+
+$CAT_COMMAND
