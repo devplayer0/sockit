d394164d0d778b70ba4229665212f53579934cd3
Jack O'Sullivan <jackos1998@gmail.com>
2019-06-20 22:09:30 +0100

    Firmware: Restart after "factory reset"

--- lua/init.lua
+++ lua/init.lua
@@ -11,9 +11,10 @@ gpio.mode(BUTTON_PIN, gpio.INT)
 gpio.trig(BUTTON_PIN, 'both', function(level, when, count)
   if level == gpio.LOW then
     button_start = when
-  elseif when - button_start > 20*1000*1000 then
-    file.remove('config.lua')
   elseif when - button_start > 5*1000*1000 then
+    if when - button_start > 20*1000*1000 then
+      file.remove('config.lua')
+    end
     node.restart()
   elseif when - button_start > 1*1000*1000 then
     print('forcing ap mode')
